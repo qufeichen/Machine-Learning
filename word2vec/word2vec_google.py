bad174df4447c921c77286ea5066317cbda65cab
qufeichen <qufei.chen@gmail.com>
2017-12-06 23:40:08 -0500

    remove unused imports

--- word2vec/word2vec_google.py
+++ word2vec/word2vec_google.py
@@ -1,11 +1,6 @@
 # simple query of google's word2vec model
 # google's pretrained model was obtained here: https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit
 import gensim
-import os
-import string
-import re
-from sklearn.feature_extraction import text
-import collections
 import timeit
 
 # start timer - used to record run time
@@ -44,13 +39,16 @@ result_3 = model.most_similar(positive=input_3, negative=input_4)
 
 # end timer
 stop_time = timeit.default_timer()
+
+
+# print results
 print('\nResults:')
 print('---------------------------------------------------')
-# print results
 print('Similarity between {} and {}:\n {}\n'.format(input_1, input_2, result_1))
 print('Similarity between {} and {}:\n {}\n'.format(input_list_1, input_list_2, result_2))
 print('Most Similar with Positive: {}, Negative: {}:\n {}\n'.format(input_3, input_4, result_3))
-# print times
+
+# print run times
 print('---------------------------------------------------')
 print('Time taken to load model: {}'.format(start_time - model_load_time))
 print('Total Execution time: {}\n'.format(start_time - stop_time))
